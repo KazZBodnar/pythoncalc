09141dc5e23e156806a7bb73bdf4a3aff74f9687
Sam Langer <57724381+KazZBodnar@users.noreply.github.com>
2019-12-11 13:03:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from math import math
 help = input("PythonCalc V 1.0; Say 'help' for more info, and 'ready' to start the calculator.")
 if help== "help" or "Help":
   print("Signs: '+', '-', '*', '/', '^2', 'sqrt', '^3', '!', 'power'.")
@@ -31,5 +32,8 @@ else:
     num2 = int(input("What is the second number?"))
     print num1**num2
   elif sign== "sqrt":
-    num1 = int(input("What is the first number?"))
-    print num1+num2
+    num1 = int(input("What is the number you seek the squae root of?"))
+    print sqrt(num1)
+  elif sign== "!":
+    num1 = int(input("What is the number you seek the factorial of?"))
+    print factorial(num1)
