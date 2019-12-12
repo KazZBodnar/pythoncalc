6bd42642b881340f5d61e7ebb4c4dea2cf76823a
Sam Langer <57724381+KazZBodnar@users.noreply.github.com>
2019-12-12 12:31:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,94 @@
-from math import math
-help = input("PythonCalc V 1.0; Say 'help' for more info, and 'ready' to start the calculator.")
+help = input("PythonCalc V 1.3; Say 'help' for available commands, and 'ready' to start the calculator. ")
 if help== "help" or "Help":
-  print("Signs: '+', '-', '*', '/', '^2', 'sqrt', '^3', '!', 'power'.")
-else:
-  sign = input("What sign should I use?")
+  print("Signs: '+', '-', '*', '/', '^2', 'sqrt', '^3', '!', 'power'. ")
+  sign = input("What operation should I use? ")
   if sign== "+":
-    num1 = int(input("What is the first number?"))
-    num2 = int(input("What is the second number?"))
-    print num1+num2
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1+num2)
   elif sign== "-":
-    num1 = int(input("What is the first number?"))
-    num2 = int(input("What is the second number?"))
-    print num1-num2
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1-num2)
   elif sign== "*":
-    num1 = int(input("What is the first number?"))
-    num2 = int(input("What is the second number?"))
-    print num1*num2
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1*num2)
   elif sign== "/":
-    num1 = int(input("What is the first number?"))
-    num2 = int(input("What is the second number?"))
-    print num1/num2
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1/num2)
   elif sign== "^2":
-    num1 = int(input("What is the number you wish to be squared?"))
-    print num1**2
+    num1 = int(input("What is the number you wish to be squared? "))
+    print("Processing...")
+    print (num1**2)
   elif sign== "^3":
-    num1 = int(input("What is the first number?"))
-    num2 = int(input("What is the second number?"))
-    print num1**3
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1**3)
   elif sign== "power":
-    num1 = int(input("What is the first number?"))
-    num2 = int(input("What is the second number?"))
-    print num1**num2
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1**num2)
   elif sign== "sqrt":
-    num1 = int(input("What is the number you seek the squae root of?"))
-    print sqrt(num1)
+    num1 = int(input("What is the number you seek the square root of? "))
+    print("Processing...")
+    print (sqrt(num1))
   elif sign== "!":
-    num1 = int(input("What is the number you seek the factorial of?"))
-    print factorial(num1)
+    num1 = int(input("What is the number you seek the factorial of? "))
+    print("Processing...")
+    print (factorial(num1))
+  else:
+    print("I am sorry, but I am unable to process your request.")
+else:
+  sign = input("What operation should I use? ")
+  if sign== "+":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1+num2)
+  elif sign== "-":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1-num2)
+  elif sign== "*":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1*num2)
+  elif sign== "/":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1/num2)
+  elif sign== "^2":
+    num1 = int(input("What is the number you wish to be squared? "))
+    print("Processing...")
+    print (num1**2)
+  elif sign== "^3":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1**3)
+  elif sign== "power":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1**num2)
+  elif sign== "sqrt":
+    num1 = int(input("What is the number you seek the square root of? "))
+    print("Processing...")
+    print (sqrt(num1))
+  elif sign== "!":
+    num1 = int(input("What is the number you seek the factorial of? "))
+    print("Processing...")
+    print (factorial(num1))
   else:
     print("I am sorry, but I am unable to process your request.")
