03a5ac3597d58a84fc8e0f0e6b161507a801e91e
Sam Langer <57724381+KazZBodnar@users.noreply.github.com>
2019-12-12 16:01:02 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
-help = input("PythonCalc V 1.3; Say 'help' for available commands, and 'ready' to start the calculator. ")
+help = input("PythonCalc V 1.3; Say 'help' for available commands, and to start the calculator. ")
 if help== "help" or "Help":
-  print("Signs: '+', '-', '*', '/', '^2', 'sqrt', '^3', '!', 'power'. ")
+  print("Signs: '+', '-', '*', '/', 'sqr', 'sqrt', 'cube', '!', 'power'. ")
+  sign = input("What operation should I use? ")
+  if sign== "+":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1+num2)
+  elif sign== "-":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1-num2)
+  elif sign== "*":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1*num2)
+  elif sign== "/":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1/num2)
+  elif sign== "sqr":
+    num1 = int(input("What is the number you wish to be squared? "))
+    print("Processing...")
+    print (num1**2)
+  elif sign== "cube":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1**3)
+  elif sign== "power":
+    num1 = int(input("What is the first number? "))
+    num2 = int(input("What is the second number? "))
+    print("Processing...")
+    print (num1**num2)
+  elif sign== "sqrt":
+    num1 = int(input("What is the number you seek the square root of? "))
+    print("Processing...")
+    print (sqrt(num1))
+  elif sign== "!":
+    num1 = int(input("What is the number you seek the factorial of? "))
+    print("Processing...")
+    print (factorial(num1))
+  else:
+    print("I am sorry, but I am unable to process your request.")
+elif help== "ready" or "Ready":
   sign = input("What operation should I use? ")
   if sign== "+":
     num1 = int(input("What is the first number? "))
@@ -47,48 +93,4 @@ if help== "help" or "Help":
   else:
     print("I am sorry, but I am unable to process your request.")
 else:
-  sign = input("What operation should I use? ")
-  if sign== "+":
-    num1 = int(input("What is the first number? "))
-    num2 = int(input("What is the second number? "))
-    print("Processing...")
-    print (num1+num2)
-  elif sign== "-":
-    num1 = int(input("What is the first number? "))
-    num2 = int(input("What is the second number? "))
-    print("Processing...")
-    print (num1-num2)
-  elif sign== "*":
-    num1 = int(input("What is the first number? "))
-    num2 = int(input("What is the second number? "))
-    print("Processing...")
-    print (num1*num2)
-  elif sign== "/":
-    num1 = int(input("What is the first number? "))
-    num2 = int(input("What is the second number? "))
-    print("Processing...")
-    print (num1/num2)
-  elif sign== "^2":
-    num1 = int(input("What is the number you wish to be squared? "))
-    print("Processing...")
-    print (num1**2)
-  elif sign== "^3":
-    num1 = int(input("What is the first number? "))
-    num2 = int(input("What is the second number? "))
-    print("Processing...")
-    print (num1**3)
-  elif sign== "power":
-    num1 = int(input("What is the first number? "))
-    num2 = int(input("What is the second number? "))
-    print("Processing...")
-    print (num1**num2)
-  elif sign== "sqrt":
-    num1 = int(input("What is the number you seek the square root of? "))
-    print("Processing...")
-    print (sqrt(num1))
-  elif sign== "!":
-    num1 = int(input("What is the number you seek the factorial of? "))
-    print("Processing...")
-    print (factorial(num1))
-  else:
-    print("I am sorry, but I am unable to process your request.")
+  print("I am sorry, but that is not a valid command.")
